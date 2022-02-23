a0d3fb56aca5f9bb532353d7016e6bb83d5f44d9
Allen <1942813644@qq.com>
2022-02-23 13:23:28 +0800

    first commit

---
+++ src/components/Child1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Child1</div>
+</template>
+
+<script>
+import log from '../mixins/log.js'
+
+export default {
+  data() {
+    return {
+      name: 'Child1'
+    }
+  },
+  created() {
+    console.log('Child 1 的 created')
+  },
+  mixins: [log]
+}
+</script>
